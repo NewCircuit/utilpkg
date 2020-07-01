58902556bb7d717ecaf7d2a7c3e177d5c0cd43e7
Dylan Hackworth <dhpf@pm.me>
2020-07-01 04:09:53 -0500

    Prompting Preview

--- bot_util.go
+++ bot_util.go
@@ -18,3 +18,41 @@ func Mention(s *Session, userID string, channelID string, context string) (*Mess
 		fmt.Sprintf("<@%s> %s", userID, context),
 	)
 }
+
+type PromptOptions struct {
+	prompt  string
+	expires int
+}
+
+func PromptUser(s *Session, message *Message, options PromptOptions) (*Message, error) {
+	return Prompt(s, message.Author.ID, message.ChannelID, options)
+}
+
+func Prompt(s *Session, userID string, channelID string, options PromptOptions) (*Message, error) {
+	resChannel := make(chan *Message)
+	var response *Message
+
+	_, err := Mention(s, userID, channelID, options.prompt)
+
+	if err != nil {
+		return nil, err
+	}
+
+	s.AddHandlerOnce(func(s *Session, msg *MessageCreate) {
+		go handlePrompt(s, msg, userID, channelID, resChannel)
+	})
+
+	response = <-resChannel
+
+	return response, nil
+}
+
+func handlePrompt(s *Session, msg *MessageCreate, userID string, channelID string, output chan *Message) {
+	if msg.ChannelID == channelID && msg.Author.ID == userID {
+		output <- msg.Message
+	} else {
+		s.AddHandlerOnce(func(s *Session, msg *MessageCreate) {
+			handlePrompt(s, msg, userID, channelID, output)
+		})
+	}
+}
