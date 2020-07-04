91cfa983f4c9049a9cbe8d8ea731e02dfe0770c9
Elian Heutink <elian0213@outlook.com>
2020-07-04 16:32:56 +0200

    Add RemoveFromSlice to common utils

--- common.go
+++ common.go
@@ -16,3 +16,18 @@ func KeepAlive() {
 func Report(context string, err error) {
 	log.Printf("%s.\n%s", context, err.Error())
 }
+
+// RemoveFromSlice takes <removeString string> <list []string>
+// RemoveFromSlice removes a specific string form a slice.
+// Returns []string
+func RemoveFromSlice(removeString string, list []string) []string {
+	indexItem := 0
+
+	for index, item := range list {
+		if item == removeString {
+			indexItem = index
+		}
+	}
+
+	return append(list[:indexItem], list[indexItem+1:]...)
+}
